45cdeae26f053eb5f7ab0104b0ad6b98267382fc
patongit <90884766+patgit12@users.noreply.github.com>
2023-10-07 01:45:41 -0400

    initial commit

---
+++ admin/variables.tf
@@ -0,0 +1,19 @@
+variable "secret_key" {
+  type = string
+  default = "RHCeiwctAmxJrzabX7qOwqWVXbP30lKCVf44a0f2"
+}
+
+variable "access_key" {
+  type = string
+  default = "AKIAZU7IMMZOURHGFDLN"
+}
+
+variable "vault_addr" {
+  type = string
+  default = "value"
+}
+
+variable "vault_token" {
+  type = string
+  default = "value"
+}
