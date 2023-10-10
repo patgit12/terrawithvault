80a6e2086829829260f1ca0197ce7f9bac705fa6
patongit <90884766+patgit12@users.noreply.github.com>
2023-10-09 22:47:46 -0400

    updating

--- admin/variables.tf
+++ admin/variables.tf
@@ -1,13 +1,3 @@
-variable "secret_key" {
-  type = string
-  default = "uUh8i4hYTmuDP4EVKpr/9owoYrYIV2WP+QmIVSTo"
-}
-
-variable "access_key" {
-  type = string
-  default = "AKIAZU7IMMZOYXY7IM4N"
-}
-
 variable "vault_addr" {
   type = string
   default = "http://127.0.0.1:8200"
